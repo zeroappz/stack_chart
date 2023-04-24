24ec5da670fe94e712e4643d879c6f38ac576039
Praveen Kumar <72295783+zeroappz@users.noreply.github.com>
2023-04-24 19:06:52 +0530

    [Example]: oPackage test with an example

--- example/stack_chart_example.dart
+++ example/stack_chart_example.dart
@@ -1,39 +1,7 @@
-import 'package:stack_chart/stack_chart.dart';
+// import 'package:stack_chart/stack_chart.dart';
 
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
-}
-
-
-// Widget stackWidget([data]) {
-//   String dd = "";
-//   if (data != null) {
-//     dd = data['type'];
-//   } else {
-//     dd = "empty";
-//   }
-//   return Column(
-//     children: [
-//       Container(
-//           margin: const EdgeInsets.only(top: 12.5),
-//           width: MediaQuery.of(context).size.width * 0.05,
-//           height: MediaQuery.of(context).size.width * 0.07,
-//           color: dd == 'impromptu'
-//               ? const Color(0xFFD9177E)
-//               : dd == 'trymd'
-//                   ? const Color(0xFFF75C1E)
-//                   : dd == 'No Slot'
-//                       ? Colors.white
-//                       : dd == 'cancel'
-//                           ? const Color(0xFFF70000)
-//                           : dd == 'available'
-//                               ? const Color(0xFF21B000)
-//                               : dd == 'empty'
-//                                   ? Colors.white
-//                                   : dd == 'telephone'
-//                                       ? const Color.fromARGB(255, 238, 226, 59)
-//                                       : const Color(0xFFE2E5EC)),
-//     ],
-//   );
+// void main() {
+//   var awesome = Awesome();
+//   print('awesome: ${awesome.isAwesome}');
 // }
+
--- lib/stack_chart.dart
+++ lib/stack_chart.dart
@@ -1,2 +1,30 @@
 library stack_chart;
+
 export 'src/stack_chart_base.dart';
+
+import 'package:flutter/material.dart';
+
+class MacinCodeButton extends StatelessWidget {
+  var onPressed;
+  final Widget child;
+  var style;
+  MacinCodeButton(
+      {Key? key, @required this.onPressed, required this.child, this.style})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+        onPressed: onPressed,
+        style: TextButton.styleFrom(
+          foregroundColor: Colors.white,
+          padding: const EdgeInsets.all(16.0),
+          backgroundColor: Colors.pinkAccent,
+          elevation: 9.0,
+          textStyle: const TextStyle(
+            fontSize: 20,
+          ),
+        ),
+        child: child);
+  }
+}
